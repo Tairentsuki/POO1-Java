51481741b6fb7a12deb9631f93c566c2ac588a84
guilherme <tairentsuki@gmail.com>
2025-08-11 10:50:11 -0300

    Adicionado ex015

---
+++ src/exLista1/ex015.java
@@ -0,0 +1,28 @@
+package exLista1;
+
+import java.util.Scanner;
+import static java.lang.Math.ceil;
+
+public class ex015 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        float coberturaTinta = 3; // m² por litro
+        float tamanhoLataEmLitros = 18;
+        float valorLata = 80;
+
+        System.out.print("Digite a área a ser pintada (M²): ");
+        float area = entrada.nextFloat();
+
+        // Quantos litros de tinta serão necessários
+        double litrosNecessarios = area / coberturaTinta;
+
+        // Quantidade de latas, arredondando para cima
+        double latasNecessarias = ceil(litrosNecessarios / tamanhoLataEmLitros);
+
+        double precoTotal = latasNecessarias * valorLata;
+
+        System.out.println("São necessárias " + (int)latasNecessarias + " latas de tinta.");
+        System.out.println("Preço total: R$ " + precoTotal);
+    }
+}
