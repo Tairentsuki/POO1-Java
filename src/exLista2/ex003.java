1a52ccf2a3c38ce09961709ce9a7d0f89bf45ca7
guilherme <tairentsuki@gmail.com>
2025-08-11 11:20:15 -0300

    adicionado ex003

---
+++ src/exLista2/ex003.java
@@ -0,0 +1,18 @@
+package exLista2;
+
+import java.util.Scanner;
+
+public class ex003 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite uma letra: ");
+        String letraDigitada = entrada.nextLine();
+
+        if(letraDigitada.equalsIgnoreCase("F")){
+            System.out.println("Você digitou F");
+        }else if(letraDigitada.equalsIgnoreCase("M")){
+            System.out.println("Você digitou M");
+        }
+    }
+}
