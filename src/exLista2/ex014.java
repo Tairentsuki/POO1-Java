b7c0dd82d3ed9219afff8ebba44dcb432dc5ff34
guilherme <tairentsuki@gmail.com>
2025-08-11 20:02:56 -0300

    adicionado ex014

---
+++ src/exLista2/ex014.java
@@ -0,0 +1,39 @@
+package exLista2;
+
+import java.util.Scanner;
+
+public class ex014 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite um número (1-7) para o dia da semana: ");
+        int dia = entrada.nextInt();
+
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Valor inválido");
+                break;
+        }
+    }
+}
