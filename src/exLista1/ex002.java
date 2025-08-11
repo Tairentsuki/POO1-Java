d6c625289a32fb2ad2919dda33492797234cdb50
guilherme <tairentsuki@gmail.com>
2025-08-11 09:41:23 -0300

    Ex 1 ao 10 da lista 1

---
+++ src/exLista1/ex002.java
@@ -0,0 +1,15 @@
+package exLista1;
+
+import java.util.Scanner;
+
+public class ex002 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite um número: ");
+		float n1 = entrada.nextFloat();
+		System.out.println("O número digitado foi: "+n1);
+		
+		entrada.close();
+	}
+}
