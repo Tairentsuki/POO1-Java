d220b3023950857ae5059808fbefd1aa163e0237
guilherme <tairentsuki@gmail.com>
2025-08-11 11:27:04 -0300

    adicionado ex004

---
+++ src/exLista2/ex005.java
@@ -0,0 +1,18 @@
+package exLista2;
+
+import java.util.Scanner;
+
+public class ex005 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite uma letra: ");
+        String letra = entrada.nextLine();
+
+        if (letra.equalsIgnoreCase("a") || letra.equals("e") || letra.equals("i") || letra.equals("o") || letra.equals("u")) {
+            System.out.println("A letra digitada é vogal.");
+        } else {
+            System.out.println("A letra digitada não é vogal.");
+        }
+    }
+}
