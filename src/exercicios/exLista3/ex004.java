170e0021ca895ddc811134ed35808bc61b1ade11
Guilherme <tairentsuki@gmail.com>
2025-08-14 09:06:25 -0300

    Adicionado ex004

---
+++ src/exercicios/exLista3/ex004.java
@@ -0,0 +1,42 @@
+//4. Supondo que a população de um país A seja da ordem de 80000 habitantes com uma taxa
+//anual de crescimento de 3% e que a população de B seja 200000 habitantes com uma taxa de
+//crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários
+//para que a população do país A ultrapasse ou iguale a população do país B, mantidas as
+//taxas de crescimento. Altere o programa anterior permitindo ao usuário informar as
+//populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a
+//operação.
+
+package exercicios.exLista3;
+
+public class ex004{
+    public static void main(String[] args){
+        double popA = 80000;
+        double taxaA = 3.0/100;
+        double popB = 200000;
+        double taxaB = 1.5/100;
+
+        double log_a_div_b = Math.log(popB / popA);
+        double log_taxaA_div_taxaB = Math.log((1 + taxaA) / (1 + taxaB));
+        double anos = Math.ceil(log_a_div_b / log_taxaA_div_taxaB);
+        System.out.println("Anos: " + anos);
+
+/*
+        int anos = 0;
+        while (popA < popB){
+            popA += popA*taxaA;
+            popB += popB*taxaB;
+            anos++;
+            System.out.println("=-=-=-=-=-=-= Log =-=-=-=-=-=-=");
+            System.out.println("CalculoTaxaA: " + popA);
+            System.out.println("PopA: " + popA);
+            System.out.println("PopB: " + popB);
+            System.out.println("Anos" + anos);
+            System.out.println("=-=-=-=-=-=-= Log =-=-=-=-=-=-=");
+        }
+*/
+
+        System.out.println("Foram necessários "+ anos + " para a população do país A ultrapassar a população B");
+        System.out.println("População do país A:" + (int) popA);
+        System.out.println("População do país B:" + (int) popB);
+    }
+}
