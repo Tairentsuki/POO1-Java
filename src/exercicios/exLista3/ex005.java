462780efb5e9b75f36b9e5a4cbb0550968a6e446
Guilherme <tairentsuki@gmail.com>
2025-08-14 09:45:49 -0300

    Ex005 feito e modificado e adicionado texto do ex.

--- src/exercicios/exLista3/ex005.java
+++ src/exercicios/exLista3/ex005.java
@@ -1,3 +1,6 @@
+//5. Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois
+//modifique o programa para que ele mostre os números um ao lado do outro.
+
 package exercicios.exLista3;
 
 public class ex005 {
