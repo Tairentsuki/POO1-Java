9d5c5fe4e2f44800ff655df048837afed8e86080
Guilherme <tairentsuki@gmail.com>
2025-08-14 10:13:24 -0300

    Ex007 feito.

---
+++ src/exercicios/exLista3/ex007.java
@@ -0,0 +1,24 @@
+//7. Faça um programa que receba dois números inteiros e gere os números inteiros que estão
+//no intervalo compreendido por eles.
+
+package exercicios.exLista3;
+
+import java.util.Scanner;
+
+public class ex007 {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o 1° número: ");
+        int n1 = entrada.nextInt();
+        System.out.print("Digite o 2° número: ");
+        int n2 = entrada.nextInt();
+
+        int valor_inicial = Math.min(n1, n2);
+        int valor_final = Math.max(n1, n2);
+
+        for(int contador = valor_inicial; contador <= valor_final; contador++){
+            System.out.println(contador);
+        }
+    }
+}
