33452a13ead1147f5a70759fe4ab244d7b8e1d79
Guilherme <tairentsuki@gmail.com>
2025-08-14 09:31:12 -0300

    Atualizado ex004

--- src/exercicios/exLista3/ex004.java
+++ src/exercicios/exLista3/ex004.java
@@ -8,12 +8,35 @@
 
 package exercicios.exLista3;
 
+import java.util.Scanner;
+
 public class ex004{
     public static void main(String[] args){
-        double popA = 80000;
-        double taxaA = 3.0/100;
-        double popB = 200000;
-        double taxaB = 1.5/100;
+
+        Scanner entrada = new Scanner(System.in);
+
+        double popA;
+        do{
+            System.out.print("Digite a população do país A: ");
+            popA = entrada.nextDouble();
+            if(popA <= 0){
+                System.out.println("\nPor favor, digite um número maior que 0. \nTente novamente.");
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+            }
+        }while(popA <= 0);
+
+        System.out.print("Digite a taxa de crescimento da população do país A (0-100)%: ");
+        double taxaA = entrada.nextDouble()/100;
+
+        double popB;
+        do{
+            System.out.print("Digite a população do país B: ");
+            popB = entrada.nextDouble();
+        }while(popB <= 0);
+
+        System.out.print("Digite a taxa de crescimento da população do país B (0-100)%: ");
+        double taxaB = entrada.nextDouble()/100;
+
 
         double log_a_div_b = Math.log(popB / popA);
         double log_taxaA_div_taxaB = Math.log((1 + taxaA) / (1 + taxaB));
@@ -37,6 +60,8 @@ public class ex004{
 
         System.out.println("Foram necessários "+ anos + " para a população do país A ultrapassar a população B");
         System.out.println("População do país A:" + (int) popA);
+        System.out.println("Taxa de crescimento do país A:" + (int) popA);
         System.out.println("População do país B:" + (int) popB);
+        System.out.println("Taxa de crescimento do país B:" + (int) popB);
     }
 }
