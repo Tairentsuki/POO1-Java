40b8b9adc4c1c8b19e8653218abff4a1ad23a536
guilherme <tairentsuki@gmail.com>
2025-08-21 22:57:49 -0300

    Feito ex003 | lista 4

---
+++ src/exercicios/exLista4/Ex003.java
@@ -0,0 +1,25 @@
+package exercicios.exLista4;
+
+import java.util.Scanner;
+
+public class Ex003{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        final int QNT_NOTAS = 4;
+
+        double[] notas = new double[QNT_NOTAS];
+
+        for(int contador = 0; contador < QNT_NOTAS; contador++){
+            System.out.print("Digite a " + (contador+1) + "° nota: ");
+            notas[contador] = entrada.nextDouble();
+        }
+        double somatorio = 0;
+        System.out.println("As notas digitadas foram: ");
+        for(double nota: notas){
+            System.out.println(nota);
+            somatorio+= nota;
+        }
+
+        System.out.println("A média é: " + (somatorio/QNT_NOTAS));
+    }
+}
