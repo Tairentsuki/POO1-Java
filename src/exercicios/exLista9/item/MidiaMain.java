786165ac00ae0cd8db56f1fc89d18df314087d1b
Guilherme <tairentsuki@gmail.com>
2025-09-23 11:15:40 -0300

    Classe MidiaMain

---
+++ src/exercicios/exLista9/item/MidiaMain.java
@@ -0,0 +1,8 @@
+package exercicios.exLista9.item;
+
+public class MidiaMain {
+    public static void main(String[] args) {
+        Midia midia = new Midia(0, "gravadora", 0, "Descricao");
+        System.out.println(midia);
+    }
+}
